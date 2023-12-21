aaa742489c33bd4dd5d78faa3990d8a9e78dea62
Kuba Kaczmarek <jakub.kaczmarek@tpa-group.pl>
2023-12-21 20:05:58 +0100

    Create VectorStore component

---
+++ src/ultimate_frisbee_rules_rag/components/vectorstore.py
@@ -0,0 +1,39 @@
+import os
+import pinecone
+from dotenv import load_dotenv
+from langchain.vectorstores import Pinecone
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import DirectoryLoader
+
+from ultimate_frisbee_rules_rag.entity.config_entity import InitialiseVectorstoreConfig
+
+
+class VectorStore:
+    def __init__(self, config: InitialiseVectorstoreConfig) -> None:
+        self.config = config
+    
+
+    def initialise(self) -> None:
+        load_dotenv()
+
+        self._load_documents()
+        self._create_index()
+        
+
+
+    def _load_documents(self) -> None:
+        loader = DirectoryLoader(self.config.documents_dir)
+        documents = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        self.splits = text_splitter.split_documents(documents)
+
+
+    def _create_index(self) -> None:
+        pinecone.init(
+            api_key=os.environ['PINECONE_API_KEY'],
+            environment=os.environ['PINECONE_ENV'],
+        )
+        
+        embeddings = OpenAIEmbeddings()
+        Pinecone.from_documents(documents=self.splits, index_name=self.config.index_name, embedding=embeddings)
